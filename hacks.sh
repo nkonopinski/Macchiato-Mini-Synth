4abe8984df0d51239e40513b7280baa5f391230a
Nathan Konopinski <n.konopinski@yahoo.com>
2025-11-22 11:54:03 -0800

    Fix envelopes
    Docs state mozzi v1.0.2 works but envelopes from this version do
    not fully turn off on note off and after some time (~30s, depending
    on release time) notes turn back on.
    Envelopes are fixed in 1.1.2. However, upgrading to  it creates audio
    artifacts when filtering both oscillators, possibly due to lack of
    cpu. For now, use ADSR.h from 1.1.2 and library v1.0.2 to get synth
    working as expected.

--- hacks.sh
+++ hacks.sh
@@ -5,5 +5,8 @@
 # Fix capacitive touch keys
 cp RCpoll.h .pio/libdeps/ATmega644P/Mozzi/RCpoll.h
 
+# Envelope fixes from v1.2.1
+cp ADSR.h .pio/libdeps/ATmega644P/Mozzi/ADSR.h
+
 # Improve audio quality
 sed -i '' "s/^#define AUDIO_MODE STANDARD_PLUS/#define AUDIO_MODE HIFI/" .pio/libdeps/ATmega644P/Mozzi/mozzi_config.h
